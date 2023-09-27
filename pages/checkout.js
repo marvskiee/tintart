8ae0d547b63ce5fd4ff11fc8aac5acfd6c8918d9
Marvi Lean Negrete <acemonkey2@gmail.com>
2023-09-27 15:52:49 +0800

    add page

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,16 +5,16 @@ import Link from 'next/link'
 const Checkout = () => {
     return (
         <CustomerLayout>
-            <div className='min-h-[40rem] flex-col gap-5 flex items-center justify-center'>
-                <div className='font-extrabold text-4xl flex flex-col items-center justify-center gap-4'>
+            <div className='min-h-[40rem] flex-col gap-5 flex items-center justify-center p-4'>
+                <div className=' font-extrabold text-4xl flex flex-col items-center justify-center gap-4'>
                     <BsCheckCircleFill className='text-emerald-500 text-6xl' />
-                    <span>
+                    <span className='text-center w-full'>
                         Checkout Successful!
                     </span>
                 </div>
-                <p className='text-lg'>Please wait for email confirmation</p>
+                <p className='text-lg text-center'>Please wait for email confirmation</p>
                 <Link href="/">
-                    <p className='underline text-md'>BACK TO HOMEPAGE</p>
+                    <p className='underline text-md text-center'>BACK TO HOMEPAGE</p>
                 </Link>
             </div>
         </CustomerLayout>
